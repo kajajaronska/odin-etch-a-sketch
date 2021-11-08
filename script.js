c2348e78d928081a5bd8fc3c668876f60a9f721b
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-11-08 09:07:05 +0000

    Created function with event listeners to imitate drawing when user hovers over tiles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const btnNewGrid = document.querySelector('.btn-new-grid');
 const gridContainer = document.querySelector('.container');
 
-// GENERATING A GRID
+// GENERATING INITAL GRID
 
 const gridSize = 16;
 
@@ -12,8 +12,33 @@ btnNewGrid.addEventListener('click', function(){
         const div = document.createElement('div');
     div.className = 'tile';
     gridContainer.appendChild(div);
+    };
 
-    }
+    draw();
+
 },{once: true});
+
+
+// GRID MOUSE EVENTS
+
+function draw(){
+    const tiles = document.querySelectorAll('.tile');
+
+    tiles.forEach((tile) => {
+        tile.addEventListener('mouseover',()=>{
+            tile.style.backgroundColor = 'black';
+            console.log('You touched a tile!!');
+        });
+    });
+
+    tiles.forEach((tile) => {
+        tile.addEventListener('mouseout',()=>{
+            tile.style.backgroundColor = 'grey';
+            console.log('You touched a tile!!');
+        });
+    });
+};
+
+
 
 
